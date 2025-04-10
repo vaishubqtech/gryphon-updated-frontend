2f8fb6e6e856c9d77e30baaa141b41c6b21d45cc
vaishubqtech <vaishnavi@bqtech.in>
2025-04-10 11:54:42 +0530

    Candlestick value updation

--- src/components/CandlestickChart/CandlestickChart.js
+++ src/components/CandlestickChart/CandlestickChart.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import "./candleChart.css"
 import { createChart } from 'lightweight-charts';
+import Web3 from 'web3';
 
 const CandlestickChart = (agentID) => {
 
@@ -22,16 +23,35 @@ const CandlestickChart = (agentID) => {
 
                 const data = await response.json();
                 console.log("chart data", data.data)
-                // Transform the API response
-                const transformed = data?.data?.map((kline) => ({
-                    time: kline.startTimeMilliseconds / 1000,
-                    open: parseFloat(kline.openPrice),
-                    high: parseFloat(kline.highestPrice),
-                    low: parseFloat(kline.lowestPrice),
-                    close: parseFloat(kline.closePrice),
+
+     
+                const tradeData = data?.data?.map((kline) => ({
+                    time: kline.startTimeMilliseconds/ 1000,
+                    open: parseFloat(kline.openPrice)*1000,
+                    high: parseFloat(kline.highestPrice)*1000,
+                    low: parseFloat(kline.lowestPrice)*1000,
+                    close: parseFloat(kline.closePrice)*1000,
+                    volume:Number(Web3.utils.fromWei(kline.tradingVolume,"ether"))
                 })).sort((a, b) => a.time - b.time);
-                console.log("transformed", transformed)
-                setTransformedData(transformed);
+
+                
+                // const tradeData= [
+                //     { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+                //     { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
+                //     { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
+                //     { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
+                //     { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
+                //     { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
+                //     { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
+                //     { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
+                //     { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
+                //     { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
+                // ]
+                
+
+
+                console.log("transformed", tradeData)
+                setTransformedData(tradeData);
                 setLoading(false);
             } catch (err) {
                 setError(err.message);
@@ -66,13 +86,17 @@ const CandlestickChart = (agentID) => {
         const candlestickSeries = chart.addCandlestickSeries({
             upColor: '#26a69a',
             downColor: '#ef5350',
-            borderVisible: false,
+            borderVisible: true, // Enable borders for better visibility
+            borderUpColor: '#26a69a',
+            borderDownColor: '#ef5350',
             wickUpColor: '#26a69a',
             wickDownColor: '#ef5350',
         });
 
-            candlestickSeries.setData(transformedData);
-            chart.timeScale().fitContent();
+  
+
+        candlestickSeries.setData(transformedData);
+        chart.timeScale().fitContent();
         // if (transformedData.length > 0) {
 
         // } else {
