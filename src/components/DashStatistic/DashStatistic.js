68f3b75f196dc3bd1e0d91aaeb61911cb9d23d97
vaishubqtech <vaishnavi@bqtech.in>
2025-04-18 16:14:39 +0530

    Market Stats UI changes

--- src/components/DashStatistic/DashStatistic.js
+++ src/components/DashStatistic/DashStatistic.js
@@ -41,29 +41,33 @@ const DashStatistic = () => {
             <div className='stats-head'>Market Overview</div>
             <div className='stats-flex'>
                 <div className='stats-container'>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <div className='cont-left-text'>Total Value Locked</div>
                         {/* <div className='cont-right-value'>0%</div> */}
                     </div>
-                    <div className='stats-main-data'>{marketValue?.tvl || 0}</div>
+                    <div className='stats-main-data'>{formatNumberStr(marketValue?.tvl ).toLocaleString()|| 0}</div>
                 </div>
                 <div className='stats-container'>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <div className='cont-left-text'>Market Cap</div>
                         {/* <div className='cont-right-value'>0%</div> */}
                     </div>
                     <div className='stats-main-data'>{marketValue?.marketCap || "0"}</div>
                 </div>
                 <div className='stats-container' style={{padding:'18px 15px'}}>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <div className='cont-left-text' style={{ paddingBottom: 15 }}>24h Change</div>
-                        <ResponsiveContainer width={70} height={40}>
+                
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+
+                    <div className='stats-main-data' style={{ paddingTop: 0, color:'#00E4A1CC' }}>{marketValue?.change24h || 0}%</div>
+                    <ResponsiveContainer width={70} height={40}>
                             <LineChart data={data}>
                                 <Line type="monotone" dataKey="value" stroke="#00C896" strokeWidth={2} dot={false} />
                             </LineChart>
                         </ResponsiveContainer>
-                    </div>
-                    <div className='stats-main-data' style={{ paddingTop: 0 }}>{marketValue?.change24h || 0}</div>
+                        </div>
                 </div>
                 {/* <div className='stats-container'>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
