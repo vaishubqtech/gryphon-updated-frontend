08c0e6d7ab67b3652d2c0d876fc14b7db0481b08
vaishubqtech <vaishnavi@bqtech.in>
2025-04-18 18:45:23 +0530

    Removed stats

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import Footer from '../../components/Footer/Footer'
 const Dashboard = ({ searchQuery }) => {
   return (
     <div className='dash-section'>
-      <DashStatistic/>
+      {/* <DashStatistic/> */}
       <AgentList searchQuery={searchQuery}/>
     </div>
   )
